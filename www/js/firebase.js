77403e060078dcf6f6e95266b7fadc4a20783d01
Talvez-ship-it <71433531+Talvez-ship-it@users.noreply.github.com>
2021-11-04 22:39:41 -0300

    Add files via upload

--- www/js/firebase.js
+++ www/js/firebase.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-	apiKey: "AIzaSyBmRn1YF2O-Z8r1ryyJ1R_KPAH3exnWxPo",
-    authDomain: "cordova-app-1bf1d.firebaseapp.com",
-    projectId: "cordova-app-1bf1d",
-    storageBucket: "cordova-app-1bf1d.appspot.com",
-    messagingSenderId: "423392315250",
-    appId: "1:423392315250:web:3fa9a0e3a679c7ec797feb",
-    measurementId: "G-V2X12V51S1"
+	apiKey: "AIzaSyCLYzyWxNdsgRV0roRgmlXdwsgwP2RNC1E",
+    authDomain: "app-agendamentos-d8e32.firebaseapp.com",
+    projectId: "app-agendamentos-d8e32",
+    storageBucket: "app-agendamentos-d8e32.appspot.com",
+    messagingSenderId: "642143321497",
+    appId: "1:642143321497:web:16089089d307d2169e6708",
+    measurementId: "G-J7JTB3LNGG"
  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
